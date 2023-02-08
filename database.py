188d99964ce03c24303b239431d3375da3b892f9
jennawhite05 <jenna.white1950@gmail.com>
2023-02-08 15:59:39 -0500

    editing files so they meet PEP-8 standards

--- database.py
+++ database.py
@@ -2,6 +2,7 @@ def create_patient_entry(patient_name, patient_mrn, patient_age):
     new_patient = [patient_name, patient_mrn, patient_age, []]
     return new_patient
 
+
 def main_driver():
     db = []
     db.append(create_patient_entry("Ann Ables", 1, 34))
@@ -18,11 +19,13 @@ def main_driver():
     testresult = get_test_result(db, 2, 'LDL')
     print(testresult)
 
+
 def print_directory(db, room_numbers):
     for i, patient in enumerate(db):
         print("Patient {} is in room {}".format(patient[0], room_numbers[i]))
-    for patient, rn in zip(db, room_numbers): #look into zip function
+    for patient, rn in zip(db, room_numbers):  # look into zip function
         print("Patient {} is in room {}".format(patient[0], rn))
+
 
 def get_patient_entry(db, mrn_to_find):
     for patient in db:
@@ -30,22 +33,25 @@ def get_patient_entry(db, mrn_to_find):
             return patient
     return False
 
+
 def add_test_to_patient(db, mrn_to_find, test_name, test_value):
     patient = get_patient_entry(db, mrn_to_find)
-    if patient == False:
+    if patient is False:
         print("Bad entry")
     else:
         patient[3].append([test_name, test_value])
-    return 
+    return
 
 # def get_test_result(db, mrn)
-    #receive medical record number 2 and  test name LDL, return test value
+    # receive medical record number 2 and  test name LDL, return test value
+
 
 def get_test_value(tests_list, test_name):
     for test in tests_list:
-        if test[0]==test_name:
-            test_value= test[1]
+        if test[0] == test_name:
+            test_value = test[1]
     return test_value
+
 
 def get_test_result(db, mrn, test_name):
     patient = get_patient_entry(db, mrn)
