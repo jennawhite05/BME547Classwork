a5e1239c921675d01738b17544ffa26d96e39f9b
jennawhite05 <jenna.white1950@gmail.com>
2023-02-15 13:06:00 -0500

    created Exception error (ValueError) and try/except

---
+++ errors.py
@@ -0,0 +1,20 @@
+# a = "The sky is blue"
+# print(a)
+
+# for letter in a:
+#     print(letter)
+
+
+def convert_string(string):
+    try:
+        number = int(string)
+        print(number)
+    except ValueError:
+        print('cannot convert to integer')
+
+def main():
+    convert_string('2')
+    convert_string('Hello')
+
+if __name__ == "__main__":
+    main()
